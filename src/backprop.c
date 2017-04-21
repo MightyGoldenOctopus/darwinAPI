607cf4b74d4649d707429a1e43a5bc2598d9dfb3
Jean-Adrien Ducastaing <jean-adrien.ducastaing@epita.fr>
2017-04-21 15:36:34 +0200

    One more minor fix

--- src/backprop.c
+++ src/backprop.c
@@ -42,7 +42,7 @@ Matrix hiddenGradient(FFNet* net, int layer, Matrix* deltaSup) {
  */
 	Matrix weights = transMatrix(&(net->layersWeights[layer]));
 	Matrix activation = sigPrime2Mat(&weights);
-	Matrix delta = multMatrix(deltaSup,&weights);
+	Matrix delta = ewmultMatrix(deltaSup,&weights);
 	delta = multMatrix(&delta,&activation);
 	Matrix dJdW = multMatrix(&(net->layersActivity[layer-1]),deltaSup);
 	*deltaSup = delta;
